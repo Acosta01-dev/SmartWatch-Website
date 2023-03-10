5913a971604ec6b8ecea357fb523d16c1e1e47ac
Cristian Acosta <cristian.acosta01@yahoo.com>
2023-03-10 01:49:39 -0300

    first commit

---
+++ app/app.js
@@ -0,0 +1,133 @@
+
+
+
+const menu = document.getElementsByClassName('burguer')[0];
+const menu_icon = document.getElementsByClassName('burguer_icon')[0];
+
+const mobile = document.getElementsByClassName('mobile_hidden');
+
+menu.addEventListener('click', function () {
+  if (mobile[0].style.display === 'block') {
+    mobile[0].style.display = 'none';
+    menu_icon.classList.replace('uil-multiply', 'uil-bars');
+  }
+  else {
+    mobile[0].style.display = 'block';
+    menu_icon.classList.replace('uil-bars', 'uil-multiply');
+  }
+});
+
+try {
+
+  const btn = document.getElementById("sharebutton");
+
+  // function for web share api
+  function webShareAPI(header, description, link) {
+    navigator
+      .share({
+        title: header,
+        text: description,
+        url: link
+      })
+      .then(() => console.log("Successful share"))
+      .catch((error) => console.log("Error sharing", error));
+  }
+
+  if (navigator.share) {
+      btn.addEventListener("click", () =>
+      webShareAPI("XYZ Smartwatch", "Discover the ultimate smartwatch for your active lifestyle with XYZ Smartwatch. Stay connected, track your fitness, and monitor your health with our advanced features and sleek design.", "www.XYZSmartwatch.com")
+    );
+  } else {
+    // Hide button if it supports webShareAPI
+    btn.style.display = "none";
+    console.error("Your Browser doesn't support Web Share API");
+    document.write("Your Browser doesn't support Web Share API");
+  }
+
+
+} catch (error) {
+  console.error('An API error ocurred:Share');
+
+}
+
+try {
+  const black = document.querySelector('#black');
+  const blue = document.querySelector('#blue');
+  const red = document.querySelector('#red');
+
+  const images = document.querySelectorAll('.imagen_topintro');
+
+  const background = document.querySelectorAll('.top');
+  const header = document.querySelectorAll('.header');
+
+
+  black.addEventListener('click', function () {
+    images.forEach(image => {
+      image.style.transform = 'translateY(-105%) rotate(10deg)';
+      image.style.opacity = '1';
+
+    });
+
+    background[0].style.backgroundColor = '#333';
+    background[1].style.backgroundColor = '#333';
+    header[0].style.backgroundColor = '#333';
+    header[0].style.borderBottom = "1px solid #333";
+
+  });
+
+
+
+  blue.addEventListener('click', function () {
+    images.forEach(image => {
+      image.style.transform = 'translateY(0%) rotate(10deg)';
+      image.style.opacity = '1';
+
+    });
+
+    background[0].style.backgroundColor = '#574fc7';
+    background[1].style.backgroundColor = '#574fc7';
+
+    header[0].style.backgroundColor = '#574fc7';
+    header[0].style.borderBottom = "1px solid #574fc7";
+
+  });
+
+  red.addEventListener('click', function () {
+    images.forEach(image => {
+      image.style.transform = 'translateY(-210%) rotate(10deg)';
+      image.style.opacity = '1';
+
+    });
+
+    background[0].style.backgroundColor = '#ff6961';
+    background[1].style.backgroundColor = '#ff6961';
+
+    header[0].style.backgroundColor = '#ff6961';
+    header[0].style.borderBottom = "1px solid  #ff6961";
+
+  });
+
+
+
+} catch (error) {
+  console.error('An error occurred while trying to use the buttons:"red","blue","black"');
+}
+try {
+  const accordionItems = document.querySelectorAll('.accordion-item');
+
+  accordionItems.forEach(item => {
+    const header = item.querySelector('.accordion-header');
+    const icon = header.querySelector('.accordion-icon');
+
+    header.addEventListener('click', () => {
+      // toggle active class on clicked item
+      item.classList.toggle('active');
+
+      // toggle icon rotation
+      icon.classList.toggle('rotate');
+    });
+  });
+} catch (error) {
+  console.error('An error on FAQ');
+}
+
---
+++ readme.md
@@ -0,0 +1,14 @@
+# SmartWatch Website
+This is a static website for a new smartwatch, showcasing its features, where to buy it, and frequently asked questions.
+
+## Pages
+- Home Page: Features high-quality images of the smartwatch and provides an overview of its features and benefits. Includes a call-to-action button that leads to the where to buy page. Next to the call-to-action button, there is a share button that allows users to share the website on their social media platforms.
+- FAQ Page: Provides answers to frequently asked questions about the smartwatch.
+- Where to Buy Page: Provides a list of official retailers where the smartwatch is available for purchase.
+
+### Technologies Used:
+This website was built using HTML, CSS, and JavaScript. The website features responsive design, although this aspect may need some work.
+
+#### License
+This website is licensed under the MIT License. Feel free to use, modify, and distribute this website for personal or commercial use.
+
